b9be21a71605b772b55ec62d68dfbf40a9a5c2b8
KLMPLS <StoigniewDagobert@gmail.com>
2024-10-14 09:47:50 +0200

    Math helper factorial

---
+++ Milczarski/introduction/build/library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o.d
@@ -0,0 +1,4 @@
+library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o: \
+ /home/stoigniew/projects/Milczarski/introduction/library/src/math_helpers.cpp \
+ /usr/include/stdc-predef.h \
+ /home/stoigniew/projects/Milczarski/introduction/library/include/math_helpers.h
