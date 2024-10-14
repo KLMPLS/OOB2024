c8094171f5b1a08856656127bbf89df825fc9c3b
KLMPLS <61095260+KLMPLS@users.noreply.github.com>
2024-10-14 09:59:40 +0200

    Delete Milczarski/introduction/build directory

--- Milczarski/introduction/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stoigniew/projects/Milczarski/introduction
-# Build directory: /home/stoigniew/projects/Milczarski/introduction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
--- Milczarski/introduction/build/library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o.d
+++
@@ -1,4 +0,0 @@
-library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o: \
- /home/stoigniew/projects/Milczarski/introduction/library/src/math_helpers.cpp \
- /usr/include/stdc-predef.h \
- /home/stoigniew/projects/Milczarski/introduction/library/include/math_helpers.h
